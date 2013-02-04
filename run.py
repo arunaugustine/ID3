b19874500452a1aeb6435c8fe30a5c0368793876
Arun Augustine <arunaugustine87@gmail.com>
2013-02-03 21:35:42 -0600

    minor changes to documentation

--- run.py
+++ run.py
@@ -4,7 +4,10 @@ from dtree import *
 """ 
 This is the entry point of the application. The main funtion that calls the 
 create decision tree algorithm is run_app. This in turn is called from main
-To run the example use python -u run.py train.dat test.dat
+
+To run
+=======
+python -u run.py train.dat test.dat
 
 Data Strucutre
 ---------------
@@ -12,7 +15,7 @@ Data Strucutre
 The funtion: create_decision_tree(examples, attributes, target_attribute, heuristic_funtion)
 takes in the following input:
 
-examples (train or test data) : list of dicts 
+examples (train or test data) : list of dicts (python dictionaries)
 attributes : list
 target_attribute: string
 heuristic_funtion: funtion pointer to "gain" funtion
